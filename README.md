a51d197003eb1a68ed5b77669278593dec99306f
Cahil Foley <cahilfoley2@gmail.com>
2022-03-29 16:46:08 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# codesandbox-react-template
-Created with CodeSandbox
+# CodeSandbox React Template
+
+A basic setup to quickly throw together tools and other prototypes using MUI v5, React and TypeScript.
---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,16 @@
+import Typography from '@mui/material/Typography'
+import Screen from '../../components/Screen'
+
+export function Home() {
+  return (
+    <Screen title="Home">
+      <Typography>
+        Welcome home - Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Recusandae veritatis maxime at esse dolore, non, cum impedit optio quae
+        sed animi exercitationem sit qui a possimus aut sequi, architecto eos.
+      </Typography>
+    </Screen>
+  )
+}
+
+export default Home
